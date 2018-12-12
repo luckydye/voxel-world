f77c2bd95be95e27b5dcb10198c946411e4ed99d
Tim Hav <tim.h4vlicek@gmail.com>
2018-12-12 01:11:24 +0100

    house keeping!

--- gl/Math.js
+++ gl/Math.js
@@ -1,46 +1,7 @@
 export class Vec {
-
 	constructor(x = 0, y = 0, z = 0) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
-
-	add(vec) {
-		if(arguments.length > 1) {
-			this.x += arguments[0];
-			this.y += arguments[1];
-			this.z += arguments[2];
-			return this;
-		}
-
-		this.x += vec.x;
-		this.y += vec.y;
-		this.z += vec.z;
-		return this;
-	}
-
-	multiply(vec) {
-		switch(typeof vec) {
-			case "object":
-				this.x *= vec.x;
-				this.y *= vec.y;
-				this.z *= vec.z;
-				return this;
-				
-			case "number":
-				this.x *= vec;
-				this.y *= vec;
-				this.z *= vec;
-				return this;
-		}
-	}
-
-	subtract(vec) {
-		this.x -= vec.x;
-		this.y -= vec.y;
-		this.z -= vec.z;
-		return this;
-	}
-
 }
