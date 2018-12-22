49039bb534e02d4c843837459d088ff3792221ac
Tim Hav <tim.h4vlicek@gmail.com>
2018-12-22 17:53:17 +0100

    brighter grid

--- src/gl/shader/GridShader.js
+++ src/gl/shader/GridShader.js
@@ -29,11 +29,11 @@ export default class GridShader extends GLShader {
                 void main () {
                     vec4 color = vec4(0, 0, 0, 1.0);
                     if(vPos.x == 0.0) {
-                        color.r = 0.5;
+                        color = vec4(1.0, 0.2, 0.2, 1.0);
                     } else if(vPos.z == 0.0) {
-                        color.b = 0.5;
+                        color = vec4(0.2, 0.2, 1.0, 1.0);
                     } else {
-                        color = vec4(0.2, 0.2, 0.2, 1.0);
+                        color = vec4(1.0, 1.0, 1.0, 1.0);
                     }
                     oFragColor = color;
                 }
