0661095a1e4b4d23d75b59ba43767474c555a4cf
Tim Havlicek <tim.h4vlicek@gmail.com>
2019-08-29 00:57:10 +0200

    new renderer prep

--- src/VoxelWorldGenerator.js
+++ src/VoxelWorldGenerator.js
@@ -1,8 +1,8 @@
 import noise from '../lib/perlin.js';
 import {
-    Group,
-    Vec,
-    Voxel
+	Group,
+	Vec,
+	Voxel
 } from '@uncut/viewport/elements';
 
 class Tile {
@@ -16,9 +16,9 @@ class Tile {
 		this.group.position.x = this.pos.x * 20;
 		this.group.position.z = this.pos.y * 20;
 
-		for(let i = 0; i < this.tileData.length; i++) {
+		for (let i = 0; i < this.tileData.length; i++) {
 			this.tileData[i] = new Array(this.height);
-			for(let j = 0; j < this.tileData[i].length; j++) {
+			for (let j = 0; j < this.tileData[i].length; j++) {
 				this.tileData[i][j] = new Array(size);
 			}
 		}
@@ -26,26 +26,26 @@ class Tile {
 }
 
 const UV = {
-	LOG: [0,0],
-	GRASS: [1,0],
-	LAVA: [2,0],
-	STONE: [3,0],
+	LOG: [0, 0],
+	GRASS: [1, 0],
+	LAVA: [2, 0],
+	STONE: [3, 0],
 
-	LEAVES: [0,1],
-	DIRT: [1,1],
-	WATER: [2,1],
-	SAND: [3,1],
+	LEAVES: [0, 1],
+	DIRT: [1, 1],
+	WATER: [2, 1],
+	SAND: [3, 1],
 }
 
 export class VoxelWorldGenerator {
-	
+
 	setSeed(n) {
 		this.seed = n;
 		noise.seed(n);
 	}
 
-	setOptions({ 
-		tileSize = 4, 
+	setOptions({
+		tileSize = 4,
 		tileHeight = 40,
 		seed = 0,
 		resolution = 15,
@@ -70,128 +70,128 @@ export class VoxelWorldGenerator {
 	generate(startpoint, TILECOUNT, put) {
 		let tileCount = 0;
 		let openSet = new Set();
-		let closedSet = createSet(TILECOUNT*2);
+		let closedSet = createSet(TILECOUNT * 2);
 		const self = this;
-	
+
 		tick(startpoint, TILECOUNT);
-	
+
 		function tick(tile, maxCount) {
-			if(tileCount > maxCount) return;
-	
+			if (tileCount > maxCount) return;
+
 			const [x, y] = tile;
 			const newTile = self.generateTile(x, y);
 			put(newTile);
-			
+
 			tileCount++;
-	
+
 			closedSet[x][y] = tile;
 			openSet.delete(tile);
-	
+
 			getNeighbors(tile);
-	
-			for(let tile of openSet) {
-				if(valid(tile)) {
+
+			for (let tile of openSet) {
+				if (valid(tile)) {
 					tick(tile, maxCount);
 				}
 			}
-	
+
 			function valid(tile) {
 				const [x, y] = tile;
 				const ctile = closedSet[x][y];
 				return !ctile;
 			}
-	
+
 			function getNeighbors() {
 				const neighbors = [
-					[x+1, y], 
-					[x-1, y], 
-					[x, y-1], 
-					[x, y+1],
+					[x + 1, y],
+					[x - 1, y],
+					[x, y - 1],
+					[x, y + 1],
 
-					[x+1, y-1], 
-					[x+1, y+1], 
-					[x-1, y-1], 
-					[x-1, y+1],
+					[x + 1, y - 1],
+					[x + 1, y + 1],
+					[x - 1, y - 1],
+					[x - 1, y + 1],
 				]
-				for(let n of neighbors) {
-					if(valid(n)) openSet.add(n);
+				for (let n of neighbors) {
+					if (valid(n)) openSet.add(n);
 				}
 			}
 		}
-	
+
 		function createSet(size) {
 			const arr = new Array(size);
-			for(let x = -size/2; x < size/2; x++) {
+			for (let x = -size / 2; x < size / 2; x++) {
 				arr[x] = new Array(size);
 			}
 			return arr;
 		}
 	}
 
-    regen(seed, callback) {
-        seed = seed || Math.random();
+	regen(seed, callback) {
+		seed = seed || Math.random();
 		this.setSeed(seed);
-		
+
 		return new Promise((resolve, reject) => {
 			const size = this.worldSize;
 
-			this.generate([0,0], size * size + 4, (newtile) => {
+			this.generate([0, 0], size * size + 4, (newtile) => {
 				callback(this.buildTile(newtile));
 			});
-			
+
 			resolve();
 		});
-    }
+	}
 
-    buildTile(tile) {
+	buildTile(tile) {
 		const tileData = tile.tileData;
-		for(let x = 0; x < tileData.length; x++) {
-			for(let y = 0; y < tileData[x].length; y++) {
-				for(let z = 0; z < tileData[x][y].length; z++) {
-					if(tileData[x][y][z]) {
+		for (let x = 0; x < tileData.length; x++) {
+			for (let y = 0; y < tileData[x].length; y++) {
+				for (let z = 0; z < tileData[x][y].length; z++) {
+					if (tileData[x][y][z]) {
 						this.voxel(tile, x, y, z, tile.pos.x, tile.pos.y);
 					}
 				}
 			}
 		}
 		return tile;
-    }
+	}
 
 	generateTile(x, y) {
-        const tileHeight = this.tileHeight;
-        const tileSize = this.tileSize;
+		const tileHeight = this.tileHeight;
+		const tileSize = this.tileSize;
 		const tile = new Tile(x, y, tileSize, tileHeight);
 		const tileData = tile.tileData;
 		const res = this.resolution;
 
 		// generate terrain
 		const material = (yvalue, x, y, z, value) => {
-			let mats = [ UV.STONE ];
+			let mats = [UV.STONE];
 
-			if(tileData[x][y-1] && tileData[x][y-1][z] == null) {
-				mats = [ UV.GRASS ];
+			if (tileData[x][y - 1] && tileData[x][y - 1][z] == null) {
+				mats = [UV.GRASS];
 			}
 
 			const dirtLayer = Math.floor(Math.random() * 2 + 2);
 
-			if (tileData[x][y-1] && tileData[x][y-1][z] == UV.GRASS ||
-				tileData[x][y-dirtLayer] && tileData[x][y-dirtLayer][z] == UV.GRASS) {
-				mats = [ UV.DIRT ];
+			if (tileData[x][y - 1] && tileData[x][y - 1][z] == UV.GRASS ||
+				tileData[x][y - dirtLayer] && tileData[x][y - dirtLayer][z] == UV.GRASS) {
+				mats = [UV.DIRT];
 			}
-			
-			if(y > tileHeight-2 && !tileData[x][y-1][z]) {
-				mats = [ UV.WATER ];
+
+			if (y > tileHeight - 2 && !tileData[x][y - 1][z]) {
+				mats = [UV.WATER];
 			}
-			if (y < tileHeight-1 && y > tileHeight-3) {
-				mats = [ UV.SAND ];
+			if (y < tileHeight - 1 && y > tileHeight - 3) {
+				mats = [UV.SAND];
 			}
 
 			return mats[Math.floor(value * mats.length)];
 		}
 
-		for(let x = 0; x < tileData.length; x++) {
-            for(let y = 0; y < tileData[x].length; y++) {
-                for(let z = 0; z < tileData[x][y].length; z++) {
+		for (let x = 0; x < tileData.length; x++) {
+			for (let y = 0; y < tileData[x].length; y++) {
+				for (let z = 0; z < tileData[x][y].length; z++) {
 
 					// gen height map
 					const noiseV = noise.perlin2((x + tile.pos.x) / res, (z + tile.pos.y) / res) * this.terrainheight;
@@ -199,9 +199,9 @@ export class VoxelWorldGenerator {
 
 					const mat = material(yvalue, x, y, z, Math.random());
 
-					if(y > yvalue-5.5) {
+					if (y > yvalue - 5.5) {
 						tileData[x][y][z] = mat;
-					} else if(y > tileHeight-2) {
+					} else if (y > tileHeight - 2) {
 						tileData[x][y][z] = mat;
 					}
 
@@ -211,107 +211,107 @@ export class VoxelWorldGenerator {
 
 						const value = noise.perlin3((x + tile.pos.x) / res, y / res, (z + tile.pos.y) / res);
 
-						if(value > this.threshold) {
+						if (value > this.threshold) {
 							tileData[x][y][z] = mat;
-						} else if(y > tileHeight-2) {
+						} else if (y > tileHeight - 2) {
 							tileData[x][y][z] = mat;
 						}
-					} else if(y > tileHeight-2) {
+					} else if (y > tileHeight - 2) {
 						tileData[x][y][z] = mat;
 					}
-                }
-            }
+				}
+			}
 		}
-		
+
 		// generate features
 		// return tile;
 
 		const treeDensity = this.treeDensity;
 
-		for(let x = 0; x < tileData.length; x++) {
-            for(let y = 0; y < tileData[x].length; y++) {
-                for(let z = 0; z < tileData[x][y].length; z++) {
+		for (let x = 0; x < tileData.length; x++) {
+			for (let y = 0; y < tileData[x].length; y++) {
+				for (let z = 0; z < tileData[x][y].length; z++) {
 					// decide if destination is valid for a tree
 
 					const treeHeight = Math.floor(Math.random() * 10 + 10);
 
-					if (x+4 < tileSize && x-4 > 0 &&
+					if (x + 4 < tileSize && x - 4 > 0 &&
 						y > treeHeight &&
-						z+4 < tileSize && z-4 > 0) {
+						z + 4 < tileSize && z - 4 > 0) {
 
-						if (tileData[x][y+1] && 
-							tileData[x][y-1] && 
-							tileData[x][y+1][z] && 
-							!tileData[x][y-1][z] &&
-							tileData[x][y+1][z] == UV.GRASS) {
+						if (tileData[x][y + 1] &&
+							tileData[x][y - 1] &&
+							tileData[x][y + 1][z] &&
+							!tileData[x][y - 1][z] &&
+							tileData[x][y + 1][z] == UV.GRASS) {
 
 							let yvalue = noise.perlin2(x * treeDensity + tile.pos.y, z * treeDensity + tile.pos.y) + 0.1;
-							
-							if(yvalue < 0.5 && yvalue > 0.45) {
+
+							if (yvalue < 0.5 && yvalue > 0.45) {
 								this.makeThing(tileData, x, y, z);
 							} else
 
-							if(yvalue >= treeDensity) {
-								this.makeTree(tileData, x, y, z, treeHeight);
-							}
+								if (yvalue >= treeDensity) {
+									this.makeTree(tileData, x, y, z, treeHeight);
+								}
 						}
 					}
 
-                }
-            }
-        }
+				}
+			}
+		}
 
 		return tile;
 	}
 
 	makeTree(tileData, x, y, z, height) {
-        const tileHeight = this.tileHeight;
+		const tileHeight = this.tileHeight;
 
 		const width = 5;
 		const bevel = 0.2;
 
-		if(y - height < tileHeight) {
+		if (y - height < tileHeight) {
 
-			if(tileData[x][y - height] && !tileData[x][y - height][z])
+			if (tileData[x][y - height] && !tileData[x][y - height][z])
 
-			for(let i = 0; i < height; i++) {
-				// make log
-				if(tileData[x][y-i]) {
-					if(i <= height-1) {
-						tileData[x][y-i][z] = UV.LOG;
-					}
-				}
-
-				// make crown
-				if(i >= 2) {
-					let diff = -i * 0.22;
-
-					if(i % 2 == 0) {
-						diff -= 2;
+				for (let i = 0; i < height; i++) {
+					// make log
+					if (tileData[x][y - i]) {
+						if (i <= height - 1) {
+							tileData[x][y - i][z] = UV.LOG;
+						}
 					}
 
-					for(let tx = -width; tx <= width; tx++) {
-						for(let ty = -width; ty <= width; ty++) {
+					// make crown
+					if (i >= 2) {
+						let diff = -i * 0.22;
 
-							if(x - tx != x || y - ty != y || i > height-2) {
+						if (i % 2 == 0) {
+							diff -= 2;
+						}
 
-								const p1 = [ x, y ];
-								const p2 = [ x - tx, y - ty ];
+						for (let tx = -width; tx <= width; tx++) {
+							for (let ty = -width; ty <= width; ty++) {
 
-								const a = p1[0] - p2[0];
-								const b = p1[1] - p2[1];
+								if (x - tx != x || y - ty != y || i > height - 2) {
 
-								const dist = Math.sqrt( a*a + b*b );
+									const p1 = [x, y];
+									const p2 = [x - tx, y - ty];
 
-								if(dist <= width + bevel + diff) {
-									tileData[x - tx][y-i][z - ty] = UV.LEAVES;
+									const a = p1[0] - p2[0];
+									const b = p1[1] - p2[1];
+
+									const dist = Math.sqrt(a * a + b * b);
+
+									if (dist <= width + bevel + diff) {
+										tileData[x - tx][y - i][z - ty] = UV.LEAVES;
+									}
+
 								}
-									
 							}
 						}
 					}
 				}
-			}
 		}
 
 	}
@@ -320,43 +320,43 @@ export class VoxelWorldGenerator {
 		// tileData[x][y][z] = UV.LAVA;
 	}
 
-    voxel(tile, x, y, z, offsetX = 0, offsetY = 0) {
+	voxel(tile, x, y, z, offsetX = 0, offsetY = 0) {
 		const tileData = tile.tileData;
-        const tileSize = this.tileSize;
+		const tileSize = this.tileSize;
 		const tileHeight = this.tileHeight;
-        const cube = new Voxel({
-            uv: tileData[x][y][z],
-            position: new Vec(
-                ((x * 20) + 10) - ((tileSize/2) * 20),
-                ((y * 20) + 10) - ((tileHeight) * 20) - 0.5,
-                ((z * 20) + 10) - ((tileSize/2) * 20),
-            )
-        });
+		const cube = new Voxel({
+			uv: tileData[x][y][z],
+			position: new Vec(
+				((x * 20) + 10) - ((tileSize / 2) * 20),
+				((y * 20) + 10) - ((tileHeight) * 20) - 0.5,
+				((z * 20) + 10) - ((tileSize / 2) * 20),
+			)
+		});
 
-        if((y-1 > 0 && y-1 < tileHeight) && tileData[x][y-1][z]) {
-            cube.visible.TOP = false;
-        }
-        if((y+1 > 0 && y+1 < tileHeight) && tileData[x][y+1][z]) {
-            cube.visible.BOTTOM = false;
-        }
-        if((z-1 > 0 && z-1 < tileSize) && tileData[x][y][z-1]) {
-            cube.visible.RIGHT = false;
-        }
-        if((z+1 > 0 && z+1 < tileSize) && tileData[x][y][z+1]) {
-            cube.visible.LEFT = false;
-        }
-        if((x-1 > 0 && x-1 < tileSize) && tileData[x-1][y][z]) {
-            cube.visible.BACK = false;
-        }
-        if((x+1 > 0 && x+1 < tileSize) && tileData[x+1][y][z]) {
-            cube.visible.FRONT = false;
-        }
+		if ((y - 1 > 0 && y - 1 < tileHeight) && tileData[x][y - 1][z]) {
+			cube.visible.TOP = false;
+		}
+		if ((y + 1 > 0 && y + 1 < tileHeight) && tileData[x][y + 1][z]) {
+			cube.visible.BOTTOM = false;
+		}
+		if ((z - 1 > 0 && z - 1 < tileSize) && tileData[x][y][z - 1]) {
+			cube.visible.RIGHT = false;
+		}
+		if ((z + 1 > 0 && z + 1 < tileSize) && tileData[x][y][z + 1]) {
+			cube.visible.LEFT = false;
+		}
+		if ((x - 1 > 0 && x - 1 < tileSize) && tileData[x - 1][y][z]) {
+			cube.visible.BACK = false;
+		}
+		if ((x + 1 > 0 && x + 1 < tileSize) && tileData[x + 1][y][z]) {
+			cube.visible.FRONT = false;
+		}
 
-        if(!cube.invisible) {
-			if(cube.buffer) {
+		if (!cube.invisible) {
+			if (cube.buffer) {
 				tile.group.add(cube);
 			}
 		}
-    }
+	}
 
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
-import { Toolbar, IconButton } from '../components/Toolbar.js';
-import { DialogBox } from '../components/Dialog.js';
-import '@uncut/console';
 import './VoxelWorld.js';
 
 window.addEventListener("DOMContentLoaded", () => onPageLod());
-window.addEventListener("contextmenu", e => e.preventDefault());
 
 function onPageLod() {
 	const world = document.querySelector('#viewport');
@@ -12,64 +8,6 @@ function onPageLod() {
 	world.onReady = () => {
 		world.createVoxelScene();
 	}
-
-	createToolbar({
-		zoomIn: IconButton({
-			icon: Icons.zoomin,
-			onclick() {
-				world.scene.camera.zoom(-1);
-			}
-		}),
-		zoomOut: IconButton({
-			icon: Icons.zoomout,
-			onclick() {
-				world.scene.camera.zoom(1);
-			}
-		}),
-
-		spacer: "spacer",
-		
-		voxel: IconButton({
-			icon: Icons.voxel,
-			onclick() {
-				openVoxelDialog(world);
-			}
-		}),
-		
-		terrain: IconButton({
-			icon: Icons.terrain,
-			onclick() {
-				openTerrainDialog(world);
-			}
-		}),
-	});
-}
-
-function openTerrainDialog(world) {
-	const dialog = new DialogBox('Terrain');
-
-	[
-		{ name: "Smoothness", id: "smoothness", default: 0.025, steps: 0.001, type: "number" },
-		{ name: "Resolution", id: "resolution", default: 50, steps: 5, type: "number" },
-		{ name: "Height", id: "height", default: 1000, steps: 100, type: "number" },
-		{ name: "Size", id: "size", default: 100, steps: 1, type: "number" }
-	].forEach(row => {
-		dialog.addField(row);
-	})
-
-	dialog.addEventListener('submit', e => {
-		world.createTerrainScene(e.detail);
-	});
-	dialog.addEventListener('change', e => {
-		const data = e.detail;
-		if(world.terrain) {
-			data.seed = world.terrain.seed;
-		}
-		if(data.size > 0) {
-			world.createTerrainScene(data);
-		}
-	});
-	document.body.appendChild(dialog);
 }
 
 function openVoxelDialog(world) {
@@ -89,43 +27,4 @@ function openVoxelDialog(world) {
 		world.createVoxelScene(data);
 	});
 	document.body.appendChild(dialog);
-}
-
-function createToolbar(buttonConfig) {
-	const toolbar = new Toolbar({ theme: "dark" });
-	for(let btn in buttonConfig) {
-		if(buttonConfig[btn] == "spacer") {
-			const spacer = document.createElement("div");
-			spacer.style.margin = "5px";
-			toolbar.appendChild(spacer);
-		} else {
-			toolbar.appendChild(buttonConfig[btn]);
-		}
-	}
-	document.getElementsByTagName("main")[0].appendChild(toolbar);
-}
-
-const Icons = {
-	voxel: `
-		<svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 308.222 326.679">
-			<defs><style>.a{fill:#d1d1d1;}.b{fill:#eee;}.c{fill:#fff;}</style></defs><g transform="translate(-653.357 -297.764)"><path class="a" d="M756.713,426.959V624.442L933.9,562.306,961.58,373.435Z"/><path class="b" d="M653.357,334.677l103.356,92.282V623.827L658.894,505.091Z"/><path class="c" d="M832.385,297.764l128.58,76.287L756.713,427.574,653.476,334.7Z"/></g>
-		</svg>
-	`,
-	terrain: `
-		<svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 583.222 375.496">
-			<defs><style>.a{fill:#cecece;}.b{fill:#e8e8e8;}</style></defs><g transform="translate(-239.318 -145.002)"><path class="a" d="M246.7,280.537c28.408,57.088,157.806,115.624,273.77,118.121s166.235-18.819,205.408-44.82,37.789-61.888-48.715-59.184S566.331,158.14,566.331,158.14s-86.5-36.494-159.492,12.165S367.283,241.164,246.7,280.537Z"/><path class="b" d="M275.123,271.124c23.979-4.85,125.565-51.186,174.351-11.2S469.469,364.7,572.64,347.9s70.38-48.786,152.757-49.586c28.854,8.059,66.382,19.133,97.143,45.587C764.1,375.9,754.867,375.9,699.5,424.5s-95.973,75.671-146.421,95.973c-79.978,1.23-66-38.129-113.2-52.908-26.7-8.518-117.875,33.222-164.754-25.839s9.105-56.6-35.805-164.262C243.19,276.793,251.144,275.975,275.123,271.124Z"/></g>
-		</svg>
-	`,
-	zoomin: `
-		<svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 294.969 282.728"><defs><style>.a{fill:#e3e3e3;}.b{fill:rgba(0,0,0,0);stroke-width:25px;}.b,.c{stroke:#e3e3e3;}.c,.e{fill:none;}.c{stroke-width:14px;}.d{stroke:none;}</style></defs><g transform="translate(-128.031 37)"><rect class="a" width="138" height="44" rx="22" transform="translate(128.031 213.03) rotate(-42)"/><g class="b" transform="translate(201 -37)"><circle class="e" cx="111" cy="111" r="98.5"/></g><g transform="translate(-19.539 -20.19)"><line class="c" x2="95.077" transform="translate(284.5 93.341)"/><line class="c" y2="93.379" transform="translate(332.039 47.5)"/></g></g></svg>
-	`,
-	zoomout: `
-		<svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 294.969 282.728"><defs><style>.a{fill:#e3e3e3;}.b{fill:rgba(0,0,0,0);stroke-width:25px;}.b,.c{stroke:#e3e3e3;}.c,.e{fill:none;}.c{stroke-width:14px;}.d{stroke:none;}</style></defs><g transform="translate(-128.031 37)"><rect class="a" width="138" height="44" rx="22" transform="translate(128.031 213.03) rotate(-42)"/><g class="b" transform="translate(201 -37)"><circle class="e" cx="111" cy="111" r="98.5"/></g><g transform="translate(-19.539 -20.19)"><line class="c" x2="95.077" transform="translate(284.5 93.341)"/></g></g></svg>
-	`,
-	rotateOn: `
-		<svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 660 284"><defs><style>.a{fill:#f8f8f8;}.b{fill:#d6d6d6;}</style></defs><g transform="translate(66 -18)"><ellipse class="a" cx="330" cy="142" rx="330" ry="142" transform="translate(-66 18)"/><ellipse class="b" cx="101" cy="44" rx="101" ry="44" transform="translate(163 104)"/></g></svg>
-	`,
-	rotateOff: `
-		<svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 660 284"><defs><style>.a{fill:#f8f8f8;}.b{fill:#d6d6d6;}</style></defs><g transform="translate(66 -18)"><ellipse class="a" cx="330" cy="142" rx="330" ry="142" transform="translate(-66 18)"/><ellipse class="b" cx="101" cy="44" rx="101" ry="44" transform="translate(163 104)"/></g></svg>
-	`,
 }
